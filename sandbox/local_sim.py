fc0e75919c8879df84ce525aaab9b2b42eb3baa6
yanajakhwal <yanajakhwal@gmail.com>
2025-12-09 18:40:55 -0500

    local sim outline

---
+++ sandbox/local_sim.py
@@ -0,0 +1,18 @@
+import random
+from dataclasses import dataclass, field
+from typing import Dict, List
+
+@dataclass
+class Agent:
+    pass
+
+@dataclass
+class Market:
+    pass
+
+@dataclass
+class World:
+    pass
+
+def main():
+    pass
